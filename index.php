5d05f55e6dc4f82ad9c36ce6d1ada58d94d9f6ff
Percy-Jackson-11 <alelebona@gmail.com>
2023-12-21 13:56:32 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Libreria</title>
+</head>
+<body>
+    <div class="contenitore">
+        <div class="nav_bar">
+
+            <div class="menu">
+            <ul>
+                <li><img src="img/library.png"  alt=""></li>
+                <li>Gialli</li>
+                <li>Fantasy</li>
+                <li>Narrativa</li>
+            </ul>
+            
+        </div>
+        <div class="sign_in"><input type="submit" name="submit" value="Login"></div>
+    </div>
+
+    <div class="conteiner_card">
+        <div class="card">
+            <div class="img"></div>
+            <div class="testo">
+                <h2>Titolo</h2>
+                <h1>autore</h1>
+                <h3>Trama</h3>
+            </div>
+        </div>
+    </div>
+
+    </div>
+</body>
+</html>
